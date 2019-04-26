62c3a9468078305ecb5ee5b94ad180e0bfb859b1
Csy817 <1075208266@qq.com>
2019-04-26 18:01:21 +0800

    购物车跳转详情页OK

--- src/pages/cart/list/index.js
+++ src/pages/cart/list/index.js
@@ -40,6 +40,12 @@ export default class List extends Component {
     // XXX 暂未实现左滑删除
   }
 
+  handleClick = (id) => {
+    Taro.navigateTo({
+      url: `/pages/detail/index?id=${id}`
+    })
+  }
+
   render () {
     const { list } = this.props
     console.log("List list",list)
@@ -57,28 +63,31 @@ export default class List extends Component {
             <Image
               className='cart-list__item-img'
               src={item.product_id.img}
+              onClick={this.handleClick.bind(this, item.product_id.id)}
             />
             <View className='cart-list__item-info'>
-              <View className='cart-list__item-title'>
-                {/*{!!item.prefix &&*/}
+              <View onClick={this.handleClick.bind(this, item.product_id.id)}>
+                <View className='cart-list__item-title'>
+                  {/*{!!item.prefix &&*/}
                   <Text className='cart-list__item-title-tag'>新人专享价</Text>
-                {/*}*/}
-                <Text className='cart-list__item-title-name' numberOfLines={1}>
-                  {item.product_id.name}
-                </Text>
-              </View>
+                  {/*}*/}
+                  <Text className='cart-list__item-title-name' numberOfLines={1}>
+                    {item.product_id.name}
+                  </Text>
+                </View>
 
-              <View className='cart-list__item-spec'>
-                <Text className='cart-list__item-spec-txt'>
-                  {item.specificationStock_id.color}
-                </Text>
+                <View className='cart-list__item-spec'>
+                  <Text className='cart-list__item-spec-txt'>
+                    {item.specificationStock_id.color}
+                  </Text>
+                </View>
               </View>
 
               <View className='cart-list__item-wrap'>
                 <Text className='cart-list__item-price'>
                   ¥{item.product_id.price}
                 </Text>
-                <View className='cart-list__item-num'>
+                <View className='cart-list__item-num' onClick={this.handleClick.bind(this, item.product_id.id)}>
                   <InputNumber
                     num={item.count}
                     onChange={this.handleUpdate.bind(this, item)}
