fd5e59cc5fa09af8168e36684f2420a0cdf16f51
Csy817 <1075208266@qq.com>
2019-05-13 14:16:00 +0800

    open-data display avatar and nickname ok

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,19 +1,23 @@
 // 请求连接前缀
-// const baseUrl = 'http://ecommerce.ioobot.cn';
-const baseUrl = 'https://ecxcx.ioobot.cn';
-// const baseUrl = 'http://ecenhance.ioobot.cn';
-// const graphqlFC = 'https://ec.ioobot.cn/graphql';            // old schema fc mongodb
-// const graphqlFC = 'http://ecenhance.ioobot.cn/graphql';      // new schema fc mongodb
-const graphqlEndpoint = baseUrl+'/graphql';      // new schema server mongodb
-const storeFile = 'http://deploy.ioobot.cn/api/store-file';
+// const baseUrl = 'http://ecommerce.ioobot.cn'
+// const baseUrl = 'http://ecenhance.ioobot.cn'
+const baseUrl = 'https://ecxcx.ioobot.cn'
+
+// graphql url
+const graphqlEndpoint = 'http://ecommerce.ioobot.cn/graphql'     // enhance server mongodb
+// const graphqlEndpoint = 'http://ecenhance.ioobot.cn/graphql'      // fc mongodb
+// const graphqlEndpoint = baseUrl+'/graphql'      // new schema server mongodb
+
+const storeFile = 'http://deploy.ioobot.cn/api/store-file'
+
 // 输出日志信息
-const noConsole = false;
+const noConsole = false
 
 //登录
-const authUrl = baseUrl + '/wx/login'
 // const authUrl = 'http://localhost:3000/wx/login'
+const authUrl = baseUrl + '/wx/login'
+
 //支付
-// const payUrl = 'http://ecenhance.ioobot.cn/payinfo'
 // const payUrl = 'http://localhost:3000/payinfo'
 const payUrl = baseUrl + '/payinfo'
 
