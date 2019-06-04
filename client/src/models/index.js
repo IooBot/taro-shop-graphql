dcc578539d2db0fbd24a4e821b799d39268df981
mike <mike@ioobot.com>
2019-06-04 16:26:23 +0800

    fix specificationStockList not work bug

--- client/src/models/index.js
+++ client/src/models/index.js
@@ -2,6 +2,7 @@ import categoryList from './categoryList';
 import slidshowList from './slideshowList';
 import productList from './productList';
 import userCartList from './userCartList';
+import specificationStockList from './specificationStockList';
 
-export default [categoryList, slidshowList, productList, userCartList];
+export default [categoryList, slidshowList, productList, userCartList, specificationStockList];
 
