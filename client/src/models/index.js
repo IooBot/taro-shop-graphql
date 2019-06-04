06106161e9ceca5876a4254a006cbdddf80d73d7
mike <mike@ioobot.com>
2019-06-04 16:31:15 +0800

    add more import of models

--- client/src/models/index.js
+++ client/src/models/index.js
@@ -3,6 +3,11 @@ import slidshowList from './slideshowList';
 import productList from './productList';
 import userCartList from './userCartList';
 import specificationStockList from './specificationStockList';
+import orderList from './orderList';
+import orderLogisticList from './orderLogisticList';
+import userAddressList from './userAddressList';
+import shortList from './shopList';
 
-export default [categoryList, slidshowList, productList, userCartList, specificationStockList];
+export default [categoryList, slidshowList, productList, userCartList, specificationStockList,
+  orderList, orderLogisticList, userAddressList, shortList];
 
